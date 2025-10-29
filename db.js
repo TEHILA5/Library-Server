c006d3d6c5d62ae6bbf7dca22c1cddd487c45485
Tehila Alon <tehila54345@gmail.com>
2025-10-29 12:17:33 +0200

    adding data base

---
+++ db.js
@@ -0,0 +1,30 @@
+
+export const books = [
+  {
+    id: 1,
+    name: "Duplicatim",
+    category: "Fantasy",
+    price: 80,
+    isBorrowed: false,
+    borrows: [] 
+  },
+  {
+    id: 2,
+    name: "Hitnakshut",
+    category: "Voltage",
+    price: 120,
+    isBorrowed: true,
+    borrows: [{ date: "2025-10-20", userId: 101 },]
+  },
+  {
+    id:3,
+    name:"Gam Ki Elech",
+    category:"Emotion",
+    price:100,
+    isBorrowed:true,
+    borrows:[
+        {date:"2025-09-30",userId:100},
+        {date:"2025-10-25",userId:102},
+    ]
+  },
+];
