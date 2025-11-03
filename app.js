82b120a15f0ec1409ef1c48966df8f3c698584f7
Tehila Alon <tehila54345@gmail.com>
2025-11-03 22:03:21 +0200

    Adding users

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { books } from "./db.js";
+import { books } from "./db_books.js";
 
 const app = express();
 app.use(express.json());
---
+++ db_users.js
@@ -0,0 +1,16 @@
+export const users = [
+  {
+    id: 100,
+    username: "Tehila",
+    email: "tehila@gmail.com",
+    password: "1234",
+    borrowedBooks: []  
+  },
+  {
+    id: 101,
+    username: "Yaakov",
+    email: "yaakov@gmail.com",
+    password: "5555",
+    borrowedBooks: []
+  }
+];
