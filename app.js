35f09dba370698225d59d79bed2914a6de6eff79
Tehila Alon <tehila54345@gmail.com>
2025-11-19 00:36:57 +0200

    adding cors and morgan

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ import { blockDays } from './middlewares/blockDays.Middleware.js';
 import { printDAte } from './middlewares/printDate.Middleware.js';
 import { notFoundHandler } from './middlewares/errors.Middleware.js';
 import { errorHandler } from './middlewares/errors.Middleware.js';
+import morgan from 'morgan';
+import cors from 'cors';
 
 dotenv.config(); 
 const app = express();
+app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
