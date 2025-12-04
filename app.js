fbcc692b2ffcc33bdadf698033be51b477e932f2
Tehila Alon <tehila54345@gmail.com>
2025-12-05 00:33:12 +0200

    Adding Mongo Connection

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import dotenv from "dotenv";
+import { config } from "dotenv";
 import bookRoutes from './routes/book.route.js';
 import userRoutes from './routes/user.route.js';
 import {addCurrentDate} from'./middlewares/date.Middleware.js';
@@ -7,11 +7,13 @@ import { blockDays } from './middlewares/blockDays.Middleware.js';
 import { printDAte } from './middlewares/printDate.Middleware.js';
 import { notFoundHandler } from './middlewares/errors.Middleware.js';
 import { errorHandler } from './middlewares/errors.Middleware.js';
+import { connectDB } from './config/db.js';
 import morgan from 'morgan';
 import cors from 'cors';
 
-dotenv.config(); 
+config(); 
 const app = express();
+connectDB();
 app.use(express.static("public"));
 app.use(morgan('dev'));
 app.use(cors());
@@ -19,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(addCurrentDate); 
-app.use(blockDays([5,6], 12, 22)); 
+app.use(blockDays([5,6], 12, 18)); 
 app.use(printDAte);
 
 app.use("/books", bookRoutes);
